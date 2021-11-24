7dd02b1b45855b8dc5f9e5a43385063d0bd7f4c0
Jani Eväkallio <jani.evakallio@gmail.com>
2021-11-24 18:12:54 +0000

    Add spell check demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from "react";
+
 import Tiptap from "./Tiptap.jsx";
-import './App.css';
+import "./App.css";
 
 const App = () => {
+  const [check, setCheck] = useState(false);
+
   return (
     <div className="App">
-      <Tiptap />
+      <div style={{ position: "absolute", top: 20, left: 20 }}>
+        <input
+          type="checkbox"
+          checked={check}
+          onChange={(e) => setCheck(e.target.checked)}
+        ></input>
+        Spellcheck
+      </div>
+      <Tiptap key={`tiptap-check-${check}`} spellcheck={check} />
     </div>
   );
 };
--- src/Tiptap.jsx
+++ src/Tiptap.jsx
@@ -1,14 +1,15 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { moby } from "./moby";
-const Tiptap = () => {
+
+const Tiptap = ({ spellcheck }) => {
   const editor = useEditor({
     extensions: [StarterKit],
     content: moby,
     onUpdate: () => {},
     editorProps: {
       attributes: {
-        spellcheck: "false",
+        spellcheck: (!!spellcheck).toString(),
       },
     },
   });
